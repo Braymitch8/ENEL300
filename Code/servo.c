91c2c3cba60e0b5d881328ad5a82ca76bed3c8f6
braydenmitchell <40307532+Braymitch8@users.noreply.github.com>
2025-04-08 11:40:58 -0600

    Update servo.c

--- Code/servo.c
+++ Code/servo.c
@@ -1,9 +1,17 @@
 /*
- * File:   servo.c
+ * File:   RFReciever.c
  * Author: braydenmitchell
  *
- * Created on March 6, 2025, 3:36 PM
+ * Created on March 23, 2025, 3:29 PM
  */
+
+
+//(UP) turns on RC PD3 using RF1
+//(DOWN) turns on RC PD4 using RF1
+//(LEFT) turns on RC PD2 using RF1
+//(RIGHT) turns on RC PD1 using RF1
+
+
 #include <avr/io.h>
 
 int main(void) {
@@ -19,8 +27,8 @@ int main(void) {
         ;
     }
     // enabled. 
-    // sets clock freq of timer/counter
-    // fTCA = f_clkper / 16 = 500kHz therefore every 2us clock counts up
+    // sets clock frequency of timer/counter
+    // fTCA = f_clock per / 16 = 500kHz therefore every 2us clock counts up
     TCA0.SINGLE.CTRLA = 0b00001001;
     
     // We will manually check the timer and reset the timer
@@ -28,27 +36,39 @@ int main(void) {
     // reset.
     TCA0.SINGLE.PER = 0xffff;
 
-    // set int timer to 1000
-    unsigned int timerThreshold = 10000;
-    unsigned int small_int = 1000;
-    //500 RIGHT 45
-    //750 MIDDLE
-    //1000 LEFT 45
+    // set timer to 1000
+    unsigned int timermax = 10000;
+    //440 RIGHT 45
+    //640 MIDDLE
+    //840 LEFT 45
    
-    PORTA.DIRSET = 0b00010000; // set PA4 as output
-    while (1) {
-        // turns PA4 on
-        PORTA.OUT |= 0b00010000;
-        
-        // while count is less than timer don't do anything
-        while( TCA0.SINGLE.CNT <= small_int) ;   
+    PORTA.DIRSET = 0b00100000; // set PA5 as output
     
-        //turns PA4 off
-        PORTA.OUT &= 0b11101111;
+    
+    while (1) {
         
-        // while count is less than timer don't do anything
-        while( TCA0.SINGLE.CNT <= timerThreshold) ; 
-        //Resets timer
-        TCA0.SINGLE.CNT = 0;
+        if (PORTD.IN & (1 << 1)) {  // PD1 is pressed (high) TURN RIGHT
+            PORTA.OUT |= 0b00100000;
+            while( TCA0.SINGLE.CNT <= 440) ; 
+            PORTA.OUT &= 0b11011111;
+            while( TCA0.SINGLE.CNT <= timermax) ; 
+            TCA0.SINGLE.CNT = 0;
+        }
+        else if (PORTD.IN & (1 << 2)) {  // PD2 is pressed (high) TURN LEFT
+            PORTA.OUT |= 0b00100000;
+            while( TCA0.SINGLE.CNT <= 840) ; 
+            PORTA.OUT &= 0b11011111;
+            while( TCA0.SINGLE.CNT <= timermax) ; 
+            TCA0.SINGLE.CNT = 0;
+        }
+        else{
+            PORTA.OUT |= 0b00100000;
+            while( TCA0.SINGLE.CNT <= 640) ; 
+            PORTA.OUT &= 0b11011111;
+            while( TCA0.SINGLE.CNT <= timermax) ; 
+            TCA0.SINGLE.CNT = 0;
+        }
+        
     }
+
 }
